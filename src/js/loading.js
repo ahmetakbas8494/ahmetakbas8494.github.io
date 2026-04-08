ea3abb460bde86d0284acfbf8f844b1339293be3
ahmetakbas8494 <147032010+ahmetakbas8494@users.noreply.github.com>
2026-04-08 20:00:51 +0200

    few changes

--- src/js/loading.js
+++ src/js/loading.js
@@ -1,10 +1,16 @@
+const loadingScreen = document.querySelector(".loading");
+const loadingWrapper = document.querySelector(".wrapper");
+const loadingText = document.querySelector(".loading-text");
+
 window.addEventListener("load", () => {
   if (type !== "mel" || isNaN(weight) || weight <= 0) {
-    alert("⚠️ Please scan QR code on the bag to access the guide.");
+    // alert("⚠️ Please scan QR code on the bag to access the guide.");
+    navigator.vibrate(200);
+    loadingText.classList.add("show");
+    loadingWrapper.classList.add("denied");
     return;
   }
-  let loadingScreen = document.querySelector(".loading");
-  document.querySelector(".wrapper div").classList.add("loaded");
+  loadingWrapper.querySelector("div").classList.add("show");
   setTimeout(() => {
     loadingScreen.innerHTML = "";
     loadingScreen.classList.add("hidden");
